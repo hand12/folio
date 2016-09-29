f6a29279a156dc598c0195ecd623cf1359bf1e5e
hand12 <hand12.k@gmail.com>
2016-09-29 22:15:02 +0900

    add references to product

---
+++ db/migrate/20160929130957_add_product_image_id_to_product.rb
@@ -0,0 +1,7 @@
+class AddProductImageIdToProduct < ActiveRecord::Migration
+  def change
+    add_reference :products, :product_image, index: true
+    add_reference :products, :college, index: true
+    add_reference :products, :lecture, index: true
+  end
+end
