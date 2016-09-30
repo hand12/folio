89f45aaa71e95d036140ff75875265cb7031c241
hand12 <hand12.k@gmail.com>
2016-09-30 15:40:48 +0900

    add image_method

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,4 +10,30 @@ class Product < ActiveRecord::Base
   def reject_images(attributed)
     attributed['image'].blank?
   end
+
+  def main_image
+    self.product_images.main.first.image
+  end
+
+  def sub_image(num)
+    self.product_images.sub[num].image
+  end
+
+  def image_boies
+    self.product_images.sub
+  end
+
+  def col_name
+    self.college.college_name if self.college.college_name?
+  end
+
+  def lec_title
+    self.lecture.lecture_title if self.lecture.lecture_title?
+  end
+
+  def college_lecture_name
+    college = self.col_name
+    lecture = self.lec_title
+    return college + "/" + lecture
+  end
 end
