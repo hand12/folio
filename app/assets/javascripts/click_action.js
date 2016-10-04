fccc118fde681272813041fb453531fb8f2a73c1
hand12 <hand12.k@gmail.com>
2016-10-05 03:49:17 +0900

    add like function

---
+++ app/assets/javascripts/click_action.js
@@ -0,0 +1,8 @@
+// $(document).on('turbolinks:load', function(){
+//   $('.shere').click(function(){
+//     $(this).css({
+//       "background-color": "#d3381c",
+//       "color": "#fff"
+//     });
+//   });
+// })
