37524379f15ebe52ae167f32063b2d4aa5fa6d9f
hand12 <hand12.k@gmail.com>
2016-10-06 01:33:22 +0900

    add preview function in new action

--- app/assets/javascripts/preview.js
+++ app/assets/javascripts/preview.js
@@ -1,27 +1,38 @@
 $(document).on("ready page:load", function(){
-    var image = "#requested_project_project_images_attributes_";
 
-    function uploadImage(image, i){
-      let image_id = image + i + "_image";
-      $(image_id).change(function(){
+  let main_image = "#product_product_images_attributes_0_image"
+
+  var i = 0
+  var images = []
+  while (i < 4){
+    images.push("#product_product_images_attributes_" + i + "_image");
+    i++;
+  }
+
+
+  function previewImage(image){
+    $(image).change(
+      function (){
         let file = $(this).prop('files')[0];
         if (file.type.match('image.*')) {
-          setImage(file,image,image_id);
+          setImage(file, image);
         } else {
           alert("画像を選択してください。");
         }
-      })
-    }
-
-    function setImage(file,image,image_id){
-      var reader = new FileReader();
-      reader.onload = function(){
-        $(image_id + "_preview").attr('src', reader.result);
       }
-      reader.readAsDataURL(file);
-    }
+    );
+  }
 
-    uploadImage(image, 0);
-    uploadImage(image, 1);
-    uploadImage(image, 2);
+  function setImage(file, image){
+    var reader = new FileReader();
+    reader.onload = function(){
+      $(image).after('<img class="preview_image">');
+      $(image).next(".preview_image").attr('src', reader.result);
+    }
+    reader.readAsDataURL(file);
+  }
+  for (i = 0; i < images.length; i++){
+    previewImage(images[i], i);
+  }
 })
+
