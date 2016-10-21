8039ddc259a8826ccd491adeed35b824e8e4a6f2
hand12 <hand12.k@gmail.com>
2016-10-21 09:54:37 +0000

    change secret

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',
-    :aws_access_key_id      => ENV['ACCESS_KEY_ID'],
-    :aws_secret_access_key  => ENV['SECRET_ACCESS_KEY'],
+    :access_key_id      => ENV['ACCESS_KEY_ID'],
+    :secret_access_key  => ENV['SECRET_ACCESS_KEY'],
     :region                 => 'ap-northeast-1'
   }
 
