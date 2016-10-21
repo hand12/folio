30a8130129b3e42359ad5e24f8cad5f4b519ded8
hand12 <hand12.k@gmail.com>
2016-10-21 12:29:01 +0900

    add setting capistrano

---
+++ config/deploy/production.rb
@@ -0,0 +1,5 @@
+# EC2サーバーのIP、EC2サーバーにログインするユーザー名、サーバーのロールを記述
+server '52.198.208.2', user: 'yusuke', roles: %w{app db web} 
+
+#デプロイするサーバにsshログインする鍵の情報を記述
+set :ssh_options, keys: '~/.ssh/first_aws_rsa' 
